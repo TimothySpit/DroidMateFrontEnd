0469166dd1d8d02e48f30cdfdc00989345aa8222
Gerrit Schelter <s9gische@stud.uni-saarland.de>
2016-01-08 15:23:39 +0100

    xml progress

--- src/com/droidmate/hook/APKExplorationInfo.java
+++ src/com/droidmate/hook/APKExplorationInfo.java
@@ -1,8 +1,40 @@
 package com.droidmate.hook;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class APKExplorationInfo {
 	
-	private boolean success;
-	private int elementsSeen;
+	private final String name;
+	private AtomicBoolean success = new AtomicBoolean(false);
+	private AtomicInteger elementsSeen = new AtomicInteger(0);
+	
+	public APKExplorationInfo(String name) {
+		super();
+		this.name = name;
+	}
 
+	public int getElementsSeen() {
+		return elementsSeen.get();
+	}
+	
+	public void addElementsSeen(int newElements) {
+		elementsSeen.addAndGet(newElements);
+	}
+
+	public void setElementsSeen(int elementsSeen) {
+		this.elementsSeen.set(elementsSeen);
+	}
+	
+	public boolean isSuccess() {
+		return success.get();
+	}
+
+	public void setSuccess(boolean success) {
+		this.success.set(success);
+	}
+
+	public String getName() {
+		return name;
+	}
 }
