275ab8ad3e0fb1f4f956a2fb63fafb7bea00a0be
Gerrit Schelter <s9gische@stud.uni-saarland.de>
2016-01-21 15:41:38 +0100

    Added confirm cancel dialog

--- WebContent/resources/js/explore/handleButtons.js
+++ WebContent/resources/js/explore/handleButtons.js
@@ -1,43 +1,54 @@
-define([ 'jquery', 'jquery.droidmate.ajax'], function(require) {
+define(
+		[ 'jquery', 'jquery.droidmate.ajax' ],
+		function(require) {
 
-	// Set output path label
-	var settings = $.droidmate.ajax.get.getDroidMateSettings();
-	$("#outputPathLabel").html(settings["outputPath"]);
-	
-	$('#outputPathLabel').click(function(e) {
-		var text = $(this).text();
-		var $this = $(this);
-		var $input = $('<input type=text>');
-		$input.prop('value', text);
-		$input.insertAfter($(this));
-		$input.focus();
-		$input.select();
-		$this.hide();
-		$input.focusout(function() {
-			$this.show();
-			$input.remove();
+			// Set output path label
+			var settings = $.droidmate.ajax.get.getDroidMateSettings();
+			$("#outputPathLabel").html(settings["outputPath"]);
+
+			$('#outputPathLabel').click(function(e) {
+				var text = $(this).text();
+				var $this = $(this);
+				var $input = $('<input type=text>');
+				$input.prop('value', text);
+				$input.insertAfter($(this));
+				$input.focus();
+				$input.select();
+				$this.hide();
+				$input.focusout(function() {
+					$this.show();
+					$input.remove();
+				});
+			});
+			// --------------------------------------
+
+			// handle StopAllButton
+			$('#stopAllBtn').click(function(e) {
+				$.droidmate.ajax.post.stopDroidMate();
+			});
+
+			// exploration button handler
+			$('#back-to-index').click(
+					function(e) {
+						bootbox.confirm(
+							"If you go back to the index page, droidmate will be stopped and all exploration results are deleted!",
+							"Cancel",
+							"Back to index",
+							function(result) {
+								// Gray out the button
+								$(this).prop("disabled", true);
+								$.droidmate.ajax.post.stopDroidMate(function(e) {
+									window.location = "/DroidMate/Index";
+								});
+							});
+
+			});
+			// --------------------------------------
+
+			// handle openFolderButton
+			$('#openFolderBtn').click(function(e) {
+				$.droidmate.ajax.post.openReportFolder();
+			});
+			// --------------------------------------
+
 		});
-	});
-	// --------------------------------------
-
-	// handle StopAllButton
-	$('#stopAllBtn').click(function(e) {
-		$.droidmate.ajax.post.stopDroidMate();
-	});
-
-	// exploration button handler
-	$('#back-to-index').click(function(e) {
-		$(this).prop("disabled", true);
-		$.droidmate.ajax.post.stopDroidMate(function(e) {
-			window.location = "/DroidMate/Index";
-		});
-	});
-	// --------------------------------------
-
-	//handle openFolderButton
-	$('#openFolderBtn').click(function(e) {
-		$.droidmate.ajax.post.openReportFolder();
-	});
-	// --------------------------------------
-
-});
