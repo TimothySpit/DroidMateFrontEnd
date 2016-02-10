43ad7c6ff2a40bacb170e380a939c305fa298c3d
Timo Speith <s9tospei@stud.uni-saarland.de>
2016-02-10 11:17:08 +0100

    DirectoryWatcher Comments

--- src/com/droidmate/processes/DirectoryWatcher.java
+++ src/com/droidmate/processes/DirectoryWatcher.java
@@ -24,6 +24,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.droidmate.interfaces.Observable;
 
+/**
+ * Observable for Directories
+ */
 public class DirectoryWatcher extends Observable<DirectoryWatcherEvent> {
 
 	private final WatchService watcher;
@@ -35,11 +38,24 @@ public class DirectoryWatcher extends Observable<DirectoryWatcherEvent> {
 
 	private Thread readinThread = null;
 
+	/**
+	 * Casts a WatchEvent to this type of WatchEvent
+	 * 
+	 * @param event the event to be casted
+	 * @return
+	 */
 	@SuppressWarnings("unchecked")
 	static <T> WatchEvent<T> cast(WatchEvent<?> event) {
 		return (WatchEvent<T>) event;
 	}
 
+	/**
+	 * Creates an new instance of the directory watcher class.
+	 * 
+	 * @param dirToWatch the directory to watch
+	 * @param recursive boolean indicating whether subfolders should be watched, too
+	 * @throws IOException if an IO error occured
+	 */
 	public DirectoryWatcher(Path dirToWatch, boolean recursive) throws IOException {
 		if (dirToWatch == null) {
 			throw new IllegalArgumentException("Directory to be watched must not be null.");
@@ -63,6 +79,12 @@ public class DirectoryWatcher extends Observable<DirectoryWatcherEvent> {
 
 	}
 
+	/**
+	 * Registers all subdirectories of the given directory
+	 * 
+	 * @param startDirectory the directory to be started with
+	 * @throws IOException
+	 */
 	private void registerAll(final Path startDirectory) throws IOException {
 		// register directory and sub-directories
 		Files.walkFileTree(startDirectory, new SimpleFileVisitor<Path>() {
@@ -74,6 +96,12 @@ public class DirectoryWatcher extends Observable<DirectoryWatcherEvent> {
 		});
 	}
 
+	/**
+	 * Registers a specific path for watching
+	 * 
+	 * @param dir the dirctory path
+	 * @throws IOException if an IO error occurs.
+	 */
 	private void register(Path dir) throws IOException {
 		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
 		keys.put(key, dir);
@@ -92,6 +120,12 @@ public class DirectoryWatcher extends Observable<DirectoryWatcherEvent> {
 		readinThread.join();
 	}
 
+	/**
+	 * Processes Events happening in the watched folders
+	 * 
+	 * @throws FileNotFoundException if one watched folder does not exist
+	 * @throws InterruptedException threadstuff
+	 */
 	void processEvents() throws FileNotFoundException, InterruptedException {
 		if (isWatchingEvents.get()) {
 			throw new IllegalStateException("Directory watcher is already started.");
@@ -108,6 +142,9 @@ public class DirectoryWatcher extends Observable<DirectoryWatcherEvent> {
 		readinThread.start();
 	}
 
+	/**
+	 * Starts reading the events. Used for liveupdate.
+	 */
 	private void startReadingEvents() {
 		while (isWatchingEvents.get()) {
 
