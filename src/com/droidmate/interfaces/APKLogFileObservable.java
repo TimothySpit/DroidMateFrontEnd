eb0e71ea904654e71a3c7b74f5bb6ba73c9896ef
Timo Speith <s9tospei@stud.uni-saarland.de>
2016-02-10 10:18:39 +0100

    Finished Interfaces Package Comments

--- src/com/droidmate/interfaces/APKLogFileObservable.java
+++ src/com/droidmate/interfaces/APKLogFileObservable.java
@@ -21,8 +21,7 @@ public class APKLogFileObservable {
 	/**
 	 * Registers an observer at this observable.
 	 * 
-	 * @param observer
-	 *            the Observer to be added
+	 * @param observer the Observer to be added
 	 */
 	public void addObserver(APKLogFileObserver observer) {
 		if (!observers.contains(observer))
@@ -32,47 +31,76 @@ public class APKLogFileObservable {
 	/**
 	 * Removes an observer from this observable
 	 * 
-	 * @param observer
-	 *            the observer to be removed
+	 * @param observer the observer to be removed
 	 */
 	public void deleteObserver(APKLogFileObserver observer) {
 		observers.remove(observer);
 	}
 
 	/**
-	 * Notifies all observers that a change happened
+	 * Notifies all observers that an exploration started
 	 * 
-	 * @param arg
-	 *            the change which happend
+	 * @param arg the exploration that started
 	 */
 	public void notifyObservers(APKExplorationStarted arg) {
 		for (APKLogFileObserver observer : observers)
 			observer.update(this, arg);
 	}
+	
+	/**
+	 * Notifies all observers that an exploration ended
+	 * 
+	 * @param arg the exploration that started
+	 */
 	public void notifyObservers(APKExplorationEnded arg) {
 		for (APKLogFileObserver observer : observers)
 			observer.update(this, arg);
 	}
 	
-	
+	/**
+	 * Notifies all observers that an exploration started
+	 * 
+	 * @param arg the exploration that started
+	 */
 	public void notifyObservers(APKStarted arg) {
 		for (APKLogFileObserver observer : observers)
 			observer.update(this, arg);
 	}
+	/**
+	 * Notifies all observers that an exploration ended
+	 * 
+	 * @param arg the exploration that ended
+	 */
 	public void notifyObservers(APKEnded arg) {
 		for (APKLogFileObserver observer : observers)
 			observer.update(this, arg);
 	}
 	
-	
+	/**
+	 * Notifies all observers that the elements seen have changed
+	 * 
+	 * @param arg the elements seen change
+	 */
 	public void notifyObservers(APKElementsSeenChanged arg) {
 		for (APKLogFileObserver observer : observers)
 			observer.update(this, arg);
 	}
+	
+	/**
+	 * Notifies all observers that the elements explored have changed
+	 * 
+	 * @param arg the elements explored change
+	 */
 	public void notifyObservers(APKElementsExploredChanged arg) {
 		for (APKLogFileObserver observer : observers)
 			observer.update(this, arg);
 	}
+	
+	/**
+	 * Notifies all observers that the screens seen have changed
+	 * 
+	 * @param arg the screens seen change
+	 */
 	public void notifyObservers(APKScreensSeenChanged arg) {
 		for (APKLogFileObserver observer : observers)
 			observer.update(this, arg);
